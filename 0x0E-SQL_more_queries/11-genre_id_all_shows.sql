782e4cf4b526deddd4232889b26e66545168e09a
Luke Tetteh <luketetteh7@gmail.com>
2023-05-09 19:18:36 +0000

    more sql queries

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,7 +1,7 @@
 -- Write a script that lists all shows contained in the database hbtn_0d_tvshows.
 
 SELECT tv_shows.title, tv_show_genres.genre_id
-	FROM tv shows
+	FROM tv_shows
 	LEFT JOIN tv_show_genres
 	ON tv_shows.id = tv_show_genres.show_id
 	LEFT JOIN tv_genres
@@ -11,8 +11,9 @@ SELECT tv_shows.title, tv_show_genres.genre_id
 
 SELECT tv_shows.title, tv_show_genres.genre_id
 	FROM tv_shows
-	LEFT JOIN tv_shows_genres
-	ON tv_shows.id = tv_shows_genres.genre_id
+	LEFT JOIN tv_show_genres
+	ON tv_shows.id = tv_show_genres.show_id
 	LEFT JOIN tv_genres
 	ON tv_genres.id = tv_show_genres.genre_id
 	ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+				
